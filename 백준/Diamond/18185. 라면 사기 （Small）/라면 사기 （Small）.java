bfbc4a34b77111250741e5fe6dba424f50c23ace
lockdoor <32711198+mij9929@users.noreply.github.com>
2023-06-22 12:57:02 +0900

    [Diamond V] Title: 라면 사기 (Small), Time: 160 ms, Memory: 15136 KB -BaekjoonHub

--- 백준/Diamond/18185. 라면 사기 （Small）/라면 사기 （Small）.java
+++ 백준/Diamond/18185. 라면 사기 （Small）/라면 사기 （Small）.java
@@ -1,15 +1,15 @@
-
 import java.util.*;
 import java.io.*;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-		int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+		int n = Integer.parseInt(st.nextToken());
 		int[] arr = new int[n+2]; // 계산의 편의를 위해 배열 크기를 +2 해
 
-		StringTokenizer st = new StringTokenizer(br.readLine());
+		st = new StringTokenizer(br.readLine());
 		for (int i = 0; i < n; i++) {
 			arr[i] = Integer.parseInt(st.nextToken());
 		}
