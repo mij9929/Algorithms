6da4f7fccafdd640f73bd414e72c1d85df491ea8
lockdoor <32711198+mij9929@users.noreply.github.com>
2023-06-24 15:16:31 +0900

    [Gold II] Title: 저울, Time: 132 ms, Memory: 14384 KB -BaekjoonHub

---
+++ 백준/Gold/2437. 저울/저울.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int n = Integer.parseInt(st.nextToken());
+		int[] arr = new int[n];
+		
+		st = new StringTokenizer(br.readLine());
+		for(int i=0; i<n; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(arr);
+		
+		int sum = 1;
+		for(int i=0; i<n; i++) {
+			if(sum < arr[i]) {
+				break;
+			}
+			sum+=arr[i];
+		}
+		
+		System.out.println(sum);
+
+	}
+}
