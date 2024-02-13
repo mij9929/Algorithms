c755caf5da653fe5f5d6170267ef87bc5de111ca
lockdoor <32711198+mij9929@users.noreply.github.com>
2024-02-13 15:56:53 +0900

    [level 3] Title: 단속카메라, Time: 12.97 ms, Memory: 65.6 MB -BaekjoonHub

---
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class Solution {
+    public int solution(int[][] routes) {
+        int answer = 0;
+        boolean[] visited = new boolean[routes[0].length];
+        Arrays.sort(routes, (a,b) -> {
+            return a[1] - b[1];
+        });
+        
+        int max = routes[0][1];
+        answer ++;
+        for(int[] route : routes){
+            if(max < route[0]) {
+                max = route[1];
+                answer++;
+            }
+            
+        }
+        
+        return answer;
+    }
+}
