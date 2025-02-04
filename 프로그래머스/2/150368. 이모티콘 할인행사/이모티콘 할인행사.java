314245fcfa8e4b54e3356a1bdac4ef3e1fc61845
lockdoor <32711198+mij9929@users.noreply.github.com>
2025-02-04 20:28:05 +0900

    [level 2] Title: 이모티콘 할인행사, Time: 101.07 ms, Memory: 94.4 MB -BaekjoonHub

--- 프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.java
+++ 프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.java
@@ -3,6 +3,11 @@ import java.util.*;
 class Solution {
     public int[] price;
     public int[] ans = new int[2];
+    public int[] percentage = new int[] {10, 20, 30, 40};
+    
+    public int applyDiscount(int price, int discountRate) {
+        return (int) (price - price * discountRate / 100.0);
+    }
     
     public int[] buy(int[][] users, int[] emoticons) {
         int[] result = new int[2];
@@ -12,10 +17,10 @@ class Solution {
             int sum = 0;
             for(int j=0; j<price.length; j++){
                 if(percent <= price[j]) {
-                    sum += (emoticons[j] - ((emoticons[j] * price[j])/100));
+                    sum += applyDiscount(emoticons[j], price[j]);
                 }
             }
-            if(sum < money) {
+            if(sum <= money) {
                 result[1] += sum;
             } else {
                 result[0]++;
@@ -36,18 +41,30 @@ class Solution {
                     ans[1] = temp[1];
                 }
             }
+
             return;
         }
         
         for(int i=idx; i<4; i++){
-            price[cnt] = (i+1) * 10;
+            price[cnt] = percentage[i];
             combination(cnt+1, idx, emoticons, users);
         }
     } 
     
     public int[] solution(int[][] users, int[] emoticons) {
         int[] answer = {};
-        price = new int[emoticons.length];        
+        price = new int[emoticons.length];
+        
+        // 목표 : 
+        // 1. 가입자 최대한 늘리기
+        // 2. 판매액 최대
+        // n명에게 m개 할인 판매 (10, 20, 30, 40%)
+        // 사용자는 본인 기준에 따라 가입
+        // - 본인 기준 일정 비율 이상 할인 이모티콘 모두 구매 -> 모두 구매한 가격이 가진 돈보다 크면 서비스 가입
+        
+        // 10부터 할인 가격 40까지 확인 (중복 순열)
+        // 가입자 수와 가격 비교 
+        
         combination(0,0, emoticons, users);
         return ans;
     }
