2c6623e44d760583f59608839d1d88908a758b75
mij9929 <32711198+mij9929@users.noreply.github.com>
2026-05-18 16:21:37 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 3.60 ms, Memory: 66.2 MB -BaekjoonHub

--- 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.java
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.java
@@ -1,21 +1,41 @@
 import java.util.*;
 
+// class Solution {
+//     public int[] solution(int[] numbers) {
+        
+        
+//         ArrayList<Integer> array = new ArrayList<>();
+        
+//         for(int i=0; i<numbers.length - 1; i++) {
+//             for(int j=1; j<numbers.length; j++) {
+//                 if(i == j) continue;
+//                 array.add(numbers[i] + numbers[j]);
+//             }
+//         }
+        
+//         int[] answer = array.stream()
+//             .sorted()
+//             .distinct()
+//             .mapToInt(a -> a.intValue())
+//             .toArray();
+        
+//         return answer;
+//     }
+// }
+
 class Solution {
     public int[] solution(int[] numbers) {
         
         
-        ArrayList<Integer> array = new ArrayList<>();
+        Set<Integer> set = new TreeSet<>();
         
         for(int i=0; i<numbers.length - 1; i++) {
-            for(int j=1; j<numbers.length; j++) {
-                if(i == j) continue;
-                array.add(numbers[i] + numbers[j]);
+            for(int j=i + 1; j<numbers.length; j++) {
+                set.add(numbers[i] + numbers[j]);
             }
         }
         
-        int[] answer = array.stream()
-            .sorted()
-            .distinct()
+        int[] answer = set.stream()
             .mapToInt(a -> a.intValue())
             .toArray();
         
