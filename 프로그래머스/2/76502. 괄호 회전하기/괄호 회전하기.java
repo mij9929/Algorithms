f0118be6aaf90b404d908528a123e5fc7546e43d
mij9929 <32711198+mij9929@users.noreply.github.com>
2026-05-19 16:52:33 +0900

    [level 2] Title: 괄호 회전하기, Time: 57.67 ms, Memory: 77.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+class Solution {
+    
+    public boolean isValid(String s, int start){
+        Stack<Character> stack = new Stack<>();
+        int len = s.length();
+        for(int i=0; i<len; i++){
+            char c = s.charAt((i+start)%len);
+            if(c == '(' || c == '[' || c == '{') stack.push(c);
+            else{
+                if(stack.isEmpty()) return false;
+                if(c == ')' && stack.pop() != '(') return false;
+                else if(c == ']' && stack.pop() != '[') return false;
+                else if(c == '}' && stack.pop() != '{') return false;
+            }
+        }
+        
+        if(stack.isEmpty()) return true;
+        return false;
+        
+    }
+    
+    public int solution(String s) {
+        int count = 0;
+        for(int i=0; i<s.length(); i++){
+            if(isValid(s, i)) count++; 
+        }
+        return count;
+    }
+}
