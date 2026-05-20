a78bd1af5156e33c88b6a0f2f90055c206671507
mij9929 <32711198+mij9929@users.noreply.github.com>
2026-05-20 13:23:08 +0900

    [level 2] Title: 괄호 회전하기, Time: 24.75 ms, Memory: 63.8 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -1,31 +1,44 @@
 import java.util.*;
 
 class Solution {
-    //
-    public boolean isValid(String s, int start){
-        Stack<Character> stack = new Stack<>();
+    public int solution(String s) {
+        int answer = 0;
+        
         int len = s.length();
         for(int i=0; i<len; i++){
-            char c = s.charAt((i+start)%len);
-            if(c == '(' || c == '[' || c == '{') stack.push(c);
-            else{
-                if(stack.isEmpty()) return false;
-                if(c == ')' && stack.pop() != '(') return false;
-                else if(c == ']' && stack.pop() != '[') return false;
-                else if(c == '}' && stack.pop() != '{') return false;
+            String rotateString = rotate(s, i, len);
+            if(isCorrect(rotateString)) {
+                answer++;
             }
         }
-        
-        if(stack.isEmpty()) return true;
-        return false;
-        
+        return answer;
     }
     
-    public int solution(String s) {
-        int count = 0;
-        for(int i=0; i<s.length(); i++){
-            if(isValid(s, i)) count++; 
+    private String rotate(String s, int start, int len) {
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i<len; i++) {
+            sb.append(s.charAt((start+i)%len));
         }
-        return count;
+        return sb.toString();
+    }
+    
+    private boolean isCorrect(String s) {
+        Stack<Character> stack = new Stack<>();
+        int len = s.length();
+        for(int i=0; i<len; i++) {
+            char c = s.charAt(i);
+            if (c == '(' || c == '[' || c == '{') {
+                stack.push(c);
+            } 
+            else {
+                if (stack.isEmpty()) return false;
+                char pop = stack.pop();
+                if (c == ')' && pop != '(') return false;
+                if (c == ']' && pop != '[') return false;
+                if (c == '}' && pop != '{') return false;
+            }
+        }    
+        
+        return stack.isEmpty();
     }
 }
