448cc8ad314bef7632eba595a35852b925e1eabb
mij9929 <32711198+mij9929@users.noreply.github.com>
2026-05-21 20:29:37 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 5.34 ms, Memory: 63.5 MB -BaekjoonHub

--- 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.java
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.util.stream.*;
 
 class Solution {
     public int solution(int[][] board, int[] moves) {
@@ -7,22 +8,12 @@ class Solution {
         Stack<Integer> bucket = new Stack<>();
         
         int size = board.length;
-        int[] boardIdx = new int[size];
-        for(int i=0; i<size; i++) {
-            boardIdx[i] = -1;
-        }
-        
-        for(int i=0; i<board[0].length; i++) {
-            for(int j=0; j<size; j++) {
-                if(board[j][i] != 0) {
-                    boardIdx[i] = j;
-                    break;
-                }
-            }
-        }
-
-        System.out.println("배열 값");
-        System.out.println(Arrays.toString(boardIdx));
+        int[] boardIdx = IntStream.range(0, size)
+                                  .map(i -> IntStream.range(0, board.length)
+                                                     .filter(j -> board[j][i] != 0)
+                                                     .findFirst()
+                                                     .orElse(board.length))
+                                  .toArray();
         
         for(int i=0; i<moves.length; i++) {
             int idx = boardIdx[moves[i]-1];
