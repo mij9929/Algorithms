5cc60b7e5d26a01478eedd1861358b864e773176
mij9929 <32711198+mij9929@users.noreply.github.com>
2026-05-21 20:18:53 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 0.86 ms, Memory: 63.6 MB -BaekjoonHub

--- 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.java
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.java
@@ -1,37 +1,45 @@
-import java.util.ArrayList;
+import java.util.*;
 
 class Solution {
     public int solution(int[][] board, int[] moves) {
         int answer = 0;
-        int result = 0;
         
-        int moveLen = moves.length;
-        int boardLen = board.length;
+        Stack<Integer> bucket = new Stack<>();
         
-        ArrayList<Integer> baguni = new ArrayList<Integer>();
+        int size = board.length;
+        int[] boardIdx = new int[size];
+        for(int i=0; i<size; i++) {
+            boardIdx[i] = -1;
+        }
         
-        for(int moveIndex=0; moveIndex<moveLen; moveIndex++){
-            for(int index=0; index < boardLen; index++){
-                int moveValue = moves[moveIndex]-1;
-                if(board[index][moveValue] > 0){
-                    baguni.add(board[index][moveValue]);
-                    board[index][moveValue] = 0;
+        for(int i=0; i<board[0].length; i++) {
+            for(int j=0; j<size; j++) {
+                if(board[j][i] != 0) {
+                    boardIdx[i] = j;
                     break;
                 }
             }
-            
-            if(baguni.size() >= 2){
-                if(baguni.get(baguni.size()-1) == baguni.get(baguni.size()-2)){
-                    baguni.remove(baguni.size()-1);
-                    baguni.remove(baguni.size()-1);
-                    result += 2;
-                }
+        }
+
+        System.out.println("배열 값");
+        System.out.println(Arrays.toString(boardIdx));
+        
+        for(int i=0; i<moves.length; i++) {
+            int idx = boardIdx[moves[i]-1];
+            if(idx >= board.length) continue;
+            if(!bucket.isEmpty() && bucket.peek() == board[idx][moves[i]-1]){
+                bucket.pop();
+                answer+=2;   
             }
+            
+            else
+                bucket.push(board[idx][moves[i]-1]);
+            
+            boardIdx[moves[i]-1]++;
         }
         
-        answer = result;
-        
-        
+
+
         return answer;
     }
 }
