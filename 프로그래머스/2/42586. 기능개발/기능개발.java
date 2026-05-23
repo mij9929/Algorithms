f48123a45ade3a6e4638256c9a61325918f8390d
mij9929 <32711198+mij9929@users.noreply.github.com>
2026-05-23 10:32:54 +0900

    [level 2] Title: 기능개발, Time: 2.94 ms, Memory: 62.8 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -1,69 +1,47 @@
 import java.util.*;
 
-class Solution {
-    static class Process{
-        public int progress;
-        public int speed;
-        public int idx;
-        
-        public Process(int progress, int speed){
-            this.progress = progress;
-            this.speed = speed;
-            this.idx = 0;
-        }
-        
-        public void working(){
-            this.progress += this.speed;
-        }
-        
-        public void finish(int idx){
-            this.idx = idx;   
-        }
-    }
-    
-    public int[] solution(int[] progresses, int[] speeds) {
-        List<Integer> answer = new ArrayList<>();
-        Queue<Process> queue = new LinkedList<>();
-        Queue<Process> complete = new LinkedList<>();
-        int len = progresses.length;
-        
-        for(int i=0; i<len; i++){
-            Process process = new Process(progresses[i], speeds[i]);
-            process.working();
-            queue.add(process);
-        }
-        
-        int idx = 0;
-        while(!queue.isEmpty()){
-            if(queue.peek().progress >= 100){
-                Process completion = queue.remove();
-                completion.finish(idx);
-                complete.add(completion);
-                len--;
-                continue;
-            }
-            idx++;
-            for(int i=0; i<len; i++){
-                Process process = queue.remove();
-                process.working();
-                queue.add(process);
-            }
-            
-        }
-        
-       
-        int[] result = new int[100];
-        while(!complete.isEmpty()){
-            Process completion = complete.remove();
-            result[completion.idx]++;
-        }
-        
-        for(int i=0; i<100; i++){
-            if(result[i] > 0)
-                answer.add(result[i]);
-        }
-        
-        return answer.stream().mapToInt(Integer::intValue).toArray();
+class Progress {
+    public int progress;
+    public int speed;
 
+    public Progress(int progress, int speed) {
+        this.progress = progress;
+        this.speed = speed;
+    }
+
+    public String toString() {
+        return " " + this.progress;
+    }
+}
+
+class Solution {
+    public int[] solution(int[] progresses, int[] speeds) {
+        int[] answer = {};
+        ArrayList<Integer> ans = new ArrayList<>();
+        int n = progresses.length;
+        Deque<Progress> queue =  new ArrayDeque<>();
+        for(int i=0; i<n; i++) {
+            Progress p = new Progress(progresses[i], speeds[i]);
+            queue.offer(p);
+        }
+
+        while(!queue.isEmpty()) {
+            for(int i=0; i<queue.size(); i++) {
+                Progress offer = queue.pollFirst();
+                offer.progress+=offer.speed;
+                queue.offer(offer);
+            }
+            int count = 0;
+
+            while(!queue.isEmpty() && queue.peek().progress >= 100) {
+                queue.poll();
+                count++;
+            }
+
+            if(count > 0)
+                ans.add(count);
+        }
+
+        return ans.stream().mapToInt(i->i).toArray();
     }
 }
