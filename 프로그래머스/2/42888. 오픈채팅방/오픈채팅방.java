804d7234eb2ba99229eace6b57da7350216a37ad
mij9929 <32711198+mij9929@users.noreply.github.com>
2026-05-24 14:42:27 +0900

    [level 2] Title: 오픈채팅방, Time: 88.62 ms, Memory: 150 MB -BaekjoonHub

---
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+class Solution {
+    public String[] solution(String[] record) {
+        HashMap<String, String> nameMap = new HashMap<>();
+        ArrayList<String> answer = new ArrayList<>();
+
+        for(String r : record) {
+            String[] split = r.split(" ");
+            String command = split[0];
+            if(command.equals("Enter")) {
+                String uid = split[1];
+                String nickname = split[2];
+                nameMap.put(uid, nickname);
+            }
+            
+            if(command.equals("Change")) {
+                String uid = split[1];
+                String nickname = split[2];
+                nameMap.put(uid, nickname);
+            }
+        }
+        
+        
+        for(String r : record) {
+            String[] split = r.split(" ");
+            String command = split[0];
+            if(command.equals("Enter")) {
+                String uid = split[1];
+                String nickname = nameMap.get(uid);
+                answer.add(nickname +"님이 들어왔습니다.");
+            }
+            
+            else if (command.equals("Leave")) {
+                String uid = split[1];
+                String nickname = nameMap.get(uid);
+                answer.add(nickname +"님이 나갔습니다."); 
+            }
+            
+        }
+        
+        return answer.toArray(new String[0]);
+    }
+}
