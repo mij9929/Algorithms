b7073e52dc0b87aa910c5f4f63fdbf1e745cff58
mij9929 <32711198+mij9929@users.noreply.github.com>
2026-05-30 18:51:34 +0900

    [level 2] Title: 예상 대진표, Time: 0.02 ms, Memory: 63 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
@@ -2,12 +2,11 @@ class Solution
 {
     public int solution(int n, int a, int b)
     {
+        // 바로 만나면 최소 1경기는 하기 때문에
         int answer = 1;
-        a-=1;
-        b-=1;
-        while(a/2 != b/2){
-            a/=2;
-            b/=2;
+        while((a+1)/2 != (b+1)/2){
+            a = (a+1)/2;
+            b = (b+1)/2;
             answer++;
         }
 
