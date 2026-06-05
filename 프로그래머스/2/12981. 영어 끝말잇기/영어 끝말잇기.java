f340799e21dc1370361f3a85adc0306f0454d76a
mij9929 <32711198+mij9929@users.noreply.github.com>
2026-06-05 21:40:40 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.15 ms, Memory: 66.8 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
@@ -3,26 +3,29 @@ import java.util.*;
 class Solution {
     public int[] solution(int n, String[] words) {
         int[] answer = new int[2];
-        int len = words.length;
-        Stack<String> stack = new Stack<>();
-        stack.push(words[0]);
-        for(int i=1; i<len; i++){
-            if(stack.contains(words[i])){
-                answer[0] = i%n + 1;
-                answer[1] = i/n + 1;
-                break;
+        ArrayDeque<String> stack = new ArrayDeque<>();
+        
+        int x = 0;
+        
+        for(String word : words) {
+            if(stack.isEmpty()) {
+                stack.push(word);          
+                x++;
+                continue;
             }
             
-            String tmp = stack.peek();
-            if(tmp.charAt(tmp.length()-1) != words[i].charAt(0)){
-                answer[0] = i%n + 1;
-                answer[1] = i/n + 1;
-                break;
-            }
-                
-            stack.push(words[i]);
+            int lastIdx = stack.peek().length()-1;
+            if(stack.peek().charAt(lastIdx) == word.charAt(0) && !stack.contains(word)) 
+                stack.push(word);
+            else 
+                return new int[]{x%n+1, x/n+1};
+            
+            x++;
         }
         
+        
+
+
         return answer;
     }
 }
