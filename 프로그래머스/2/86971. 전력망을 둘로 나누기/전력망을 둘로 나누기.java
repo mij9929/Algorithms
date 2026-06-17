3fe38224e5855b09b46452e49c430cc3e8757f14
mij9929 <32711198+mij9929@users.noreply.github.com>
2026-06-17 13:36:17 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 1.13 ms, Memory: 82.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -0,0 +1,66 @@
+import java.util.*;
+import java.lang.*;
+
+
+class Solution {
+    
+    public int[] parent;
+    public int diff = 0;
+    public List<Integer>[] list;
+    public int solution(int n, int[][] wires) {
+        int answer = Integer.MAX_VALUE;
+        
+        list = new ArrayList[n+1];
+        parent = new int[n+1];
+        
+        for(int i=1; i<=n; i++) {
+            list[i] = new ArrayList<>();
+            parent[i] = i;
+        }
+        
+        for(int i=0; i<wires.length; i++) {
+            int[] newParent = new int[n+1];
+            
+            for(int k=1; k<=n; k++) {
+                newParent[k] = k;
+            }
+            
+            for(int j=0; j<wires.length; j++) {
+                if(j == i) continue;
+                
+                int from = wires[j][0];
+                int to = wires[j][1];
+                union(from, to, newParent);
+            }
+            
+            int num1 = newParent[1];
+            int c1 = 0, c2 =0;
+            for(int k=1; k<=n; k++) {
+                if(num1 == find(newParent[k], newParent)) c1++;
+                else c2++;
+            }
+            
+            answer = Math.min(answer, diff(c1,c2));
+        }
+        
+        return answer;
+    }
+    
+    public void union(int x, int y, int[] newParent) {
+        int a = find(x, newParent);
+        int b = find(y, newParent);
+        
+        if(a <= b) newParent[b] = a;
+        else newParent[a] = b;
+    }
+    
+    public int find(int x, int[] newParent) {
+        if(x == newParent[x]) return x;
+        return newParent[x] = find(newParent[x], newParent);
+    }
+    
+    public int diff(int a, int b) {
+        if(a >= b) return a-b;
+        return b-a;
+    }
+}
