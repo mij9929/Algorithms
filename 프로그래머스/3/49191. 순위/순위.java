0b6e14aaa62b659f980e70c3341deb3cfb96c846
mij9929 <32711198+mij9929@users.noreply.github.com>
2026-06-17 17:52:56 +0900

    [level 3] Title: 순위, Time: 5.46 ms, Memory: 80.5 MB -BaekjoonHub

--- 프로그래머스/3/49191. 순위/순위.java
+++ 프로그래머스/3/49191. 순위/순위.java
@@ -1,44 +1,6 @@
-import java.util.*;
-
 class Solution {
     public int solution(int n, int[][] results) {
-        boolean[][] win = new boolean[n+1][n+1];
-        
-        for(int[] result: results) {
-            int winner = result[0];
-            int loser = result[1];
-            
-            win[winner][loser] = true;
-        }
-        
-        for(int k=1; k<=n; k++) {
-            for(int i=1; i<=n; i++) {
-                for(int j=1; j<=n; j++) {
-                    if(win[i][k] && win[k][j]) {
-                        win[i][j] = true;
-                    }
-                }
-            }
-        }
-        
         int answer = 0;
-        
-        for(int i=1; i<=n; i++) {
-            int known = 0;
-            
-            for(int j=1; j<=n; j++) {
-                if(i==j) continue;
-                
-                if(win[i][j] || win[j][i]) {
-                    known ++;
-                }
-            }
-            
-            if(known == n-1) {
-                answer++;
-            }
-        }
-        
         return answer;
     }
 }
