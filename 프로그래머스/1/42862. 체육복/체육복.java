03496aac9df9eb2f95f75c7ab4b6fd940811811a
mij9929 <32711198+mij9929@users.noreply.github.com>
2026-06-20 02:27:00 +0900

    [level 1] Title: 체육복, Time: 0.03 ms, Memory: 73.7 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.java
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -1,39 +1,41 @@
+import java.util.*;
+
 class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
-        int answer = 0;
+        int[] students = new int[n+1];
+        Arrays.fill(students, 1);
+        students[0] = 0;
         
-        int[] arr = new int[n];
-        
-        for(int i=0; i<n; i++){
-            arr[i] = 1;
+        for(int i=0; i<lost.length; i++) {
+            int num = lost[i];
+            students[num]--;
         }
         
-        for(int i=0; i<lost.length; i++){
-            arr[lost[i] - 1] -= 1;
+        for(int i=0; i<reserve.length; i++) {
+            int num = reserve[i];
+            students[num] ++;
         }
-        
-        for(int i=0; i<reserve.length; i++){
-            arr[reserve[i]- 1] += 1;
-        }
-        
-        for(int i=0; i<arr.length; i++){
-            if(arr[i] == 2){
-                if(i-1 > -1 && arr[i-1] == 0){
-                    arr[i] -=1;
-                    arr[i-1] += 1;
+
+        for(int i=1; i<=n; i++) {
+            if(students[i] == 1) continue;
+            
+            if(students[i] == 0) {
+                if(i-1 > 0 &&students[i-1] > 1 ) {
+                    students[i-1] --;
+                    students[i] ++;
                 }
                 
-                else if(i+1 < arr.length && arr[i+1] == 0 ){
-                    arr[i] -=1;
-                    arr[i+1] += 1;
+                else if(i+1 <= n && students[i+1] > 1) {
+                    students[i+1] --;
+                    students[i] ++;
                 }
             }
         }
         
-        for(int i=0; i<arr.length; i++){
-            if(arr[i] >= 1){
-                answer++;
-            }
+        int answer = 0;
+        
+        for(int i=1; i<=n; i++) {
+            if(students[i] >= 1) answer++;
         }
         
         return answer;
