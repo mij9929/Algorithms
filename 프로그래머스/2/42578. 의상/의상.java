4ce6b86f0f3e156c75f998f667ebf5f72e042fe1
mij9929 <32711198+mij9929@users.noreply.github.com>
2026-07-12 22:49:22 +0900

    [level 2] Title: 의상, Time: 0.08 ms, Memory: 72.9 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -1,20 +1,7 @@
-import java.util.*;
-
 class Solution {
     public int solution(String[][] clothes) {
-        int answer = 1;
-        
-        Map<String, Integer> map = new HashMap<>();
-        
-        for(String[] cloth : clothes) {
-            map.put(cloth[1], map.getOrDefault(cloth[1], 0) + 1);
-        }
-        
-        // 옷 경우의 수 + 선택 안하는 경우(+1)
-        for(int count : map.values()) {
-            answer *= (count + 1);
-        }
-        
-        return answer - 1; // 아무것도 입지 않는 경우(-1)
+        int answer = 0;
+        a b c
+        return answer;
     }
 }
