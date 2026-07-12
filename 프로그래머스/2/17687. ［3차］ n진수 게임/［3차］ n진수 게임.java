f033c3085bdb680576ded9ae3d5ec9bb28ff12a5
mij9929 <32711198+mij9929@users.noreply.github.com>
2026-07-12 20:43:19 +0900

    [level 2] Title: [3차] n진수 게임, Time: 16.61 ms, Memory: 93.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    public String solution(int n, int t, int m, int p) {
+        StringBuilder answer = new StringBuilder();
+        StringBuilder sb = new StringBuilder();
+        
+        for(int i=0; i<t*m; i++) {
+            String c = String.valueOf(Integer.toString(i, n)).toUpperCase();
+            sb.append(c);
+        }
+        
+        String numbers = sb.toString();
+        int len = numbers.length();
+        
+        for(int i=0; i<len; i++) {
+            if(i%m + 1 == p) answer.append(numbers.charAt(i));
+            if(answer.length() == t) break;
+        }
+        
+        return answer.toString();
+    }
+}
